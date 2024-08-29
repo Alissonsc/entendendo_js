e471e0f12dcf709750f64d0c590af347b1246be8
alissonsc <alisson.csilva16@senacsp.edu.br>
2024-08-29 20:10:40 -0300

    feat: exercíccio 13

---
+++ ex13.js
@@ -0,0 +1,15 @@
+/*13. Faça um algoritmo que leia um nome de usuário e a sua senha. A senha não pode ser igual ao 
+nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.*/
+
+let usuario = prompt("Informe o nome de usuário");
+let senha = prompt("Informe a senha");
+let mensagem = "";
+if (usuario == senha) {
+    alert("A senha não pode ser a mesma que o usuário.");
+    let = usuario = prompt("Informe o nome de usuário");
+    let = senha = prompt("Informe a senha");
+}
+while ((usuario) != (senha)) {
+    alert("Usuário e senha aceitos.");
+    break;
+} 
