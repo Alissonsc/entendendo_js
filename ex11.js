e4a2b32109270e142b5cfd14ed2cacbc16665c6a
alissonsc <alisson.csilva16@senacsp.edu.br>
2024-08-29 19:16:00 -0300

    feat: exercícios 11 e 12 corrigidos

--- ex11.js
+++ ex11.js
@@ -1,21 +1,25 @@
-let textoMenu = `Escolha o tipo de conversão desejada:
-                1 - Celsus
-                2 - Fahrenheit`;
+let infoMenu =
+    ` Escolha uma uma conversão a fazer:
+    1- De Celsius para Fahrenheit
+    2- De Fahrenheit para Celsius
+`;
+let celsius = 0, fahrenheit = 0;
+let opcao = prompt(infoMenu);
 
-let temp = 0, celsius = fahrenheit = 0;
-let opcao = prompt(textoMenu);
-
-switch (opcao)) {
-    case 1:
-        celsius = parseInt("Informe quantos graus celssius a converter!");
-        fahrenheit = (celsius * 9/5) + 32;
-        alert(`${celsius} °C representa ${fahrenheit.toFixed(2)}°F!`);
-        
+switch (opcao) {
+    case "1":
+        celsius = parseFloat(
+            prompt("Informe quantos graus celsius a converter!"));
+        fahrenheit = (celsius * 9 / 5) + 32;
+        alert(`${celsius}ºC representa ${fahrenheit.toFixed(2)}ºF!`);
         break;
-    case 2:
-        fahrenheit = parseInt("Informe quantos graus fahrenheit a converter!");
+    case "2":
+        fahrenheit = parseFloat(
+            prompt("Informe quantos graus fahrenheit a converter!"));
         celsius = (fahrenheit - 32) * 5 / 9;
-        alert(`${fahrenheit}`)
+        alert(`${fahrenheit}ºF representa ${celsius.toFixed(2)}ºC!`);
+        break;
     default:
+        alert("Opção Inválida!");
         break;
 }
